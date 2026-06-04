9bd57514ac2871851f940c1845cb561422758194
Tuti Wulandari <75082730+tutiwulandari@users.noreply.github.com>
2026-06-05 00:55:33 +0700

    Update DBConnection.java

--- src/test/java/com/ayo/utils/DBConnection.java
+++ src/test/java/com/ayo/utils/DBConnection.java
@@ -1,16 +1,10 @@
-/**
- * DANA Indonesia
- * Copyright (c) 2018‐2026 All Rights Reserved.
- */
+
 package com.ayo.utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-/**
- * @author hibatullaha
- * @version $Id: DBConnection.java, v 0.1 2026‐06‐04 23.59 hibatullaha Exp $$
- */
+
 public class DBConnection {
     private static final String URL =
             "jdbc:mysql://localhost:3306/ayo_db";
